157cf285ba61b954a9d1ac4c1e89fd5f0e6e6738
Moonline <moonline@users.noreply.github.com>
2023-10-04 18:01:34 +0200

    WIP database

--- src/database.tf
+++ src/database.tf
@@ -5,3 +5,23 @@ resource "google_firestore_database" "concerts_database" {
   location_id = var.region
   type        = "FIRESTORE_NATIVE"
 }
+
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/firestore_field
+resource "google_firestore_field" "concerts_database_artist_field" {
+  project = var.project
+  database = google_firestore_database.concerts_database.id
+  collection = "concerts_%{random_suffix}"
+  field = "artist"
+
+  index_config {
+    indexes {
+        order = "ASCENDING"
+        query_scope = "COLLECTION_GROUP"
+    }
+    indexes {
+        array_config = "CONTAINS"
+    }
+  }
+
+  ttl_config {}
+}
